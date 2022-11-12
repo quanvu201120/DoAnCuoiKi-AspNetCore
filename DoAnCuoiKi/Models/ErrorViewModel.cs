fa321de07fa4ad22d8d337ff3311b9a37ba00311
quan <vuquan201120@gmail.com>
2022-11-12 11:34:48 +0700

    Add project files.

---
+++ DoAnCuoiKi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAnCuoiKi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
