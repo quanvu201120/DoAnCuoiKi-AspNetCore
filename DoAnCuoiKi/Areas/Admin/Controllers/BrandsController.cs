2e0f089b7746783d13a8045f46437af7abb9c668
quan <vuquan201120@gmail.com>
2022-11-13 17:22:42 +0700

    add addmin, header nav home, nav category

---
+++ DoAnCuoiKi/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoAnCuoiKi.Data;
+
+namespace DoAnCuoiKi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public BrandsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Brands
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.brands.ToListAsync());
+        }
+
+        // GET: Admin/Brands/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.brands
+                .FirstOrDefaultAsync(m => m.brandId == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // GET: Admin/Brands/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Brands/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("brandId,name")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Admin/Brands/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Admin/Brands/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("brandId,name")] Brand brand)
+        {
+            if (id != brand.brandId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(brand);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandExists(brand.brandId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Admin/Brands/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.brands
+                .FirstOrDefaultAsync(m => m.brandId == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // POST: Admin/Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.brands == null)
+            {
+                return Problem("Entity set 'MyDbContext.brands'  is null.");
+            }
+            var brand = await _context.brands.FindAsync(id);
+            if (brand != null)
+            {
+                _context.brands.Remove(brand);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandExists(int id)
+        {
+          return _context.brands.Any(e => e.brandId == id);
+        }
+    }
+}
---
+++ DoAnCuoiKi/Models/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DoAnCuoiKi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnCuoiKi.Models.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly MyDbContext _context;
+        public CategoryViewComponent(MyDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var listCate = _context.categories.ToList();
+            return View("CateViewComponent", listCate);
+        }
+    }
+}
