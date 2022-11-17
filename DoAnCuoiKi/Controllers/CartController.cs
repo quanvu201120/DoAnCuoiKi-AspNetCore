ce9875892d07be07eff0a8af1044889263b22144
quan <vuquan201120@gmail.com>
2022-11-17 07:22:25 +0700

    add cart

---
+++ DoAnCuoiKi/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using DoAnCuoiKi.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DoAnCuoiKi.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly MyDbContext myDbContext;
+
+        public CartController(MyDbContext myDbContext)
+        {
+            this.myDbContext = myDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = HttpContext.User.Claims.FirstOrDefault(item => item.Type == "userId").Value;
+
+            var myCarts = myDbContext.carts.Where(item => item.userId.ToString() == userId).ToList();
+
+            return View(myCarts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HanleSubmitCart([Bind("customerName,customerAddress ,customerEmail,customerPhone ,otherInformation ,totalPrice")] Order order)
+        {
+            order.dateCreate = DateTime.Now;
+            var userId = HttpContext.User.Claims.FirstOrDefault(item => item.Type == "userId").Value;
+
+            order.userId = int.Parse(userId);
+
+            myDbContext.orders.Add(order);
+            await myDbContext.SaveChangesAsync();
+
+            var orderId = order.orderId;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- DoAnCuoiKi/Controllers/ProductDetailController.cs
+++ DoAnCuoiKi/Controllers/ProductDetailController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnCuoiKi.Controllers
 {
+    [Authorize]
     public class ProductDetailController : Controller
     {
         private readonly MyDbContext _context;
@@ -13,7 +14,6 @@ namespace DoAnCuoiKi.Controllers
             _context = context;
         }
 
-        [Authorize]
         public IActionResult Index()
         {
             //params url
@@ -27,6 +27,27 @@ namespace DoAnCuoiKi.Controllers
             }
 
             return View(product);
+        }
+
+        [HttpPost]
+        public string HandleAddProductToCart(string productId, int count)
+        {
+            var product = _context.products.SingleOrDefault(item => item.productId.ToString() == productId);
+            var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(item => item.Type == "userId").Value);
+
+            var cartAdd = new Cart
+            {
+                name = product.name,
+                price = product.price,
+                productId = int.Parse(productId),
+                userId = userId,
+                amount = count,
+                
+            };
+            _context.carts.Add(cartAdd);
+            _context.SaveChanges();
+
+            return "true";
         }
     }
 }
